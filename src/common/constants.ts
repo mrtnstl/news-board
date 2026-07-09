0a7a185ec350d16fd98856de2808521087c0a941
Márton Spiteller <spitellermarton@gmail.com>
2026-07-09 21:01:06 +0200

    refactor: moved PAGE_NUMBER_TEMPL to /common/constants and types used by PuppeteerScraper to /types

---
+++ src/common/constants.ts
@@ -0,0 +1,1 @@
+export const PAGE_NUMBER_TEMPL = "<%PAGE_NUM%>";
--- src/types/scraper.types.ts
+++ src/types/scraper.types.ts
@@ -1,3 +1,33 @@
+export interface IScraper<T> {
+    init(url: string): Promise<void>;
+    scrape(): Promise<T[]>;
+    cleanup(): Promise<void>;
+}
+
+export type ScrapeField = {
+    selector: string;
+    attribute?: string;
+    fallback?: string;
+};
+
+export type DataSourcesMap = {
+    nytimes: string;
+    bleepingcomputer: string;
+};
+
+// options type for puppeteer scraper
+export type PuppeteerOptions<T> = {
+    url: string;
+    currentPage: number;
+    maxPages: number;
+    selectorToWaitFor?: string;
+    elementsRoot: string;
+    rawFields: Record<Exclude<keyof T, "index">, ScrapeField>;
+};
+
+export type ScraperOptions = PuppeteerOptions<unknown>;
+
+// response type for news data themed scraper
 export type TScraperNewsResponse = {
     index: string;
     title: string;
