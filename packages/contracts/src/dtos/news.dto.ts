e87c98f0f6791aa2f7daf49a2f149620885b24bc
Márton Spiteller <spitellermarton@gmail.com>
2026-07-17 15:34:47 +0200

    add(packages/contracts): create and update types derived from INews interface

--- packages/contracts/src/dtos/news.dto.ts
+++ packages/contracts/src/dtos/news.dto.ts
@@ -5,6 +5,7 @@ export const NewsStatus = {
     failed: "failed",
 } as const;
 
+// contract for mongodb and zod schemas
 export interface INews {
     articleId: string;
     source: string;
@@ -32,17 +33,28 @@ export interface INews {
 }
 
 export type UpdateNewsStatus = Partial<
-    | Pick<INews, "status">
-    | Pick<INews, "status" | "error" | "failedAt" | "retryCount">
+    Pick<INews, "status" | "error" | "failedAt" | "retryCount">
 >;
 
-// Pick but get property from one level deep nested objects
-type DeepPickerL1<T, K1 extends keyof T, K2 extends keyof T[K1]> = {
-    [P in K2]: T[K1][K2];
-};
+export type CreateNews = Pick<
+    INews,
+    | "articleId"
+    | "source"
+    | "url"
+    | "originalUrl"
+    | "topic"
+    | "isArchievable"
+    | "status"
+    | "summary"
+    | "scraperConfigId"
+    | "publishedAt"
+    | "scrapedAt"
+> &
+    UpdateNewsStatus;
 
-export type UpdateNewsWithClassification =
-    | DeepPickerL1<INews, "classification", "sentimentScore">
-    | DeepPickerL1<INews, "classification", "readingTimeSeconds">;
+export type UpdateNews = UpdateNewsStatus & UpdateNewsClassification;
 
-export type UpdateNewsProcessedAt = Pick<INews, "processedAt">;
+export type UpdateNewsClassification = Pick<
+    INews,
+    "processedAt" | "classification"
+>;
